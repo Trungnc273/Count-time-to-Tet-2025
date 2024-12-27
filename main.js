5922c5a7d05fef29639391ee3e4e4fe24dca5b67
Trungnc273 <yamatel2004@gmail.com>
2024-12-27 11:37:53 +0700

    11

---
+++ main.js
@@ -0,0 +1,19 @@
+const tet = new Date("1-29-2025 0:0:0");
+function countTime() {
+  let currentDate = new Date();
+  let time = tet.getTime() - currentDate.getTime();
+  let ngay = time / (1000 * 60 * 60 * 24);
+  let gio = (ngay % Math.floor(ngay)) * 24;
+  let phut = (gio % Math.floor(gio)) * 60;
+  let giay = (phut % Math.floor(phut)) * 60;
+  document.querySelector("#time").innerHTML =
+    Math.floor(ngay) +
+    " ngày " +
+    Math.floor(gio) +
+    " giờ " +
+    Math.floor(phut) +
+    " phút " +
+    Math.floor(giay) +
+    " giây";
+}
+let loop = setInterval(countTime, 1000);
